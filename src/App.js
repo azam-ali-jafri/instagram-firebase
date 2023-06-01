5ab575b3cef3452d811a30dc4a56ea4cd1a7545f
Azam Ali <azamalijafrii@gmail.com>
2023-06-01 20:00:00 +0530

    test commit

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ function App() {
         <Route path="/" element={<Dashboard />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
